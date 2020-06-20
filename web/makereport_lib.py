02deed0cc25f2741850b83294a7c5a517084010c
Matt Groth <mgroth49@gmail.com>
2020-06-20 18:01:11 -0400

    add makereport

---
+++ web/makereport_lib.py
@@ -0,0 +1,25 @@
+# **** upload URLs will need to not be static since apparently wolfram can't really handle uploads to the same url over and over. but redirects can be used, and make different kinds of cloud objects too
+
+from wolframclient.language import wl
+
+from mlib.boot.mlog import log
+from mlib.boot.mutil import log_invokation, Temp
+from mlib.wolf.wolfpy import WOLFRAM
+
+def update_report(new_url, pub_url):
+    co = WOLFRAM.cloud_deploy(wl.HTTPRedirect(new_url), url=pub_url, public=True)
+    log(f'finished deploying redirect:{co[0]}')
+
+RESOURCES_ROOT = "https://www.wolframcloud.com/obj/mjgroth/Resources/"
+
+@log_invokation(with_args=True)
+def upload_webpage(htmlDoc, wolfFolder, permissions='Private', resource_folder=None, DEV=False):
+    if DEV:
+        wolfFolder = 'Dev/' + wolfFolder
+    with Temp('temp.html', w=htmlDoc.getCode()) as t:
+        co = WOLFRAM.copy_file(t, f'{wolfFolder}/index.html', permissions=permissions)
+    with Temp('temp.css', w=htmlDoc.stylesheet) as t:
+        WOLFRAM.copy_file(t, f'{wolfFolder}/style.css', permissions=permissions)
+    if resource_folder is not None:
+        WOLFRAM.copy_file(resource_folder, f'Resources/{wolfFolder}', permissions=permissions)
+    return co[0]
