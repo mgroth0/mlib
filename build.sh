f29a556b18752f81cd217346cf1b75b24a8b4b71
Matt Groth <mgroth49@gmail.com>
2020-06-22 17:29:21 -0400

    working with build script

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ oldPWD=$(pwd)
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPTPATH
 rm -rf *.eeg-info ; echo test ; rm -rf build ; rm -rf dist ;
-"$HOME"/miniconda3/envs/mlib/bin/python -m pip freeze > reqs_pip.txt
+"$HOME"/miniconda3/envs/mlib/bin/pip freeze > reqs_pip.txt
 "$HOME"/miniconda3/bin/conda list -n mlib -e > reqs_conda.txt; sed -i '' '/pypi/d' reqs_conda.txt
 
 # checks git is not dirty
-"$HOME"/miniconda3/bin/python -m bumpversion patch # minor,major
+"$HOME"/miniconda3/envs/build/bin/python -m bumpversion patch # minor,major
 
 
 git add .bumpversion.cfg
@@ -16,8 +16,8 @@ git add setup.py
 git commit -m "auto-commit build cfgs"
 git push
 #"$HOME"/miniconda3/bin/python -m incremental.update mlib --patch
-"$HOME"/miniconda3/bin/python setup.py sdist bdist_wheel
-"$HOME"/miniconda3/bin/python -m twine check dist/*
-"$HOME"/miniconda3/bin/python -m twine upload --repository testpypi dist/*
+"$HOME"/miniconda3/envs/build/bin/python setup.py sdist bdist_wheel
+"$HOME"/miniconda3/envs/build/bin/python -m twine check dist/*
+"$HOME"/miniconda3/envs/build/bin/python -m twine upload --repository testpypi dist/*
 #"--record" "files2.txt"
 cd $oldPWD
