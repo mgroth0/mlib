760e07e14dabb54cfb32a13e1115b5a6e77dd34d
Matt Groth <mgroth49@gmail.com>
2020-06-22 17:47:18 -0400

    auto-commit build cfgs

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def list_reqs():
     return reqs
 setuptools.setup(
     name="mlib-mgroth0",
-    version="0.0.14",
+    version="0.0.15",
     author="Matt Groth",
     author_email="mjgroth@mit.edu",
     description="Matt's lib",
