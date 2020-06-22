e11190cee0b29b6a14c0979d8b2e201fe7cc8ead
Matt Groth <mgroth49@gmail.com>
2020-06-22 19:58:53 -0400

    pw buildscript

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 # I think this is run in a weird temp dir during conda-build
-echo "inside build.sh in:"
+echo "build.sh: start"
+echo "build.sh: pwd:"
 pwd
-echo "ls -a:"
+echo "build.sh: ls:"
 ls -a
 #"$HOME"/miniconda3/envs/build/bin/python setup.py sdist bdist_wheel
+echo "build.sh: conda install"
 conda install --file reqs_conda.txt
+echo "build.sh: pip install"
 python -m pip install --no-deps --ignore-installed .
 #"$HOME"/miniconda3/envs/build/bin/python -m twine check dist/*
 #$PYTHON setup.py install
-echo "finished build.sh"
+echo "build.sh: finished"
