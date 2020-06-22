307eeaa17ba38923ff245282488309ee027d7ee9
Matt Groth <mgroth49@gmail.com>
2020-06-22 19:10:22 -0400

    pw buildscript

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 # I think this is run in a weird temp dir during conda-build
-echo "inside build.sh in ($pwd)"
-echo "ls: ($ls)"
+echo "inside build.sh in:"
+pwd
+echo "ls:"
+ls
 $PYTHON setup.py install
 echo "finished build.sh"
