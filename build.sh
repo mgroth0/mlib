1bfddd01f517e40a784dacf011cb77ac8bc19e53
Matt Groth <mgroth49@gmail.com>
2020-06-22 14:54:53 -0400

    pypi prep

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-exec "$HOME/miniconda3/bin/python" "setup.py" "sdist" "bdist_wheel"
+set -e
+rm -rf *.eeg-info ; echo test ; rm -rf build ; rm -rf dist ;
+"$HOME"/miniconda3/bin/python -m bumpversion micro
+#"$HOME"/miniconda3/bin/python -m incremental.update mlib --patch
+"$HOME"/miniconda3/bin/python setup.py sdist bdist_wheel
+"$HOME"/miniconda3/bin/python -m twine check dist/*
+"$HOME"/miniconda3/bin/python -m twine upload --repository testpypi dist/*
 #"--record" "files2.txt"
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-    name="mlib",  # Replace with your own username
+    name="mlib-mgroth0",
     version="0.0.1",
     author="Matt Groth",
     author_email="mjgroth@mit.edu",
@@ -15,7 +15,11 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: MacOS X",
+        "Operating System :: MacOS :: MacOS X",
     ],
     python_requires='>=3.8',
+
+    # use_incremental=True,
+    # setup_requires=['incremental'],
+    # install_requires=['incremental']  # along with any other install dependencies
 )
