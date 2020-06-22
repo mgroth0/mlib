8d74c1477ae5c57bccb57fc674af3e897f7832be
Matt Groth <mgroth49@gmail.com>
2020-06-22 15:06:44 -0400

    pypi prep

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 set -e
 rm -rf *.eeg-info ; echo test ; rm -rf build ; rm -rf dist ;
 "$HOME"/miniconda3/bin/python -m bumpversion patch # minor,major
+git add .bumpversion.cfg
+git add setup.py
+git commit -m "auto-commit build cfgs"
 #"$HOME"/miniconda3/bin/python -m incremental.update mlib --patch
 "$HOME"/miniconda3/bin/python setup.py sdist bdist_wheel
 "$HOME"/miniconda3/bin/python -m twine check dist/*
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="mlib-mgroth0",
-    version="0.0.1",
+    version="0.0.2",
     author="Matt Groth",
     author_email="mjgroth@mit.edu",
     description="Matt's lib",
