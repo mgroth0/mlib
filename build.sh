65669ce0360e026f649725207d79facefc9da2e4
Matt Groth <mgroth49@gmail.com>
2020-06-22 15:03:40 -0400

    pypi prep

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 rm -rf *.eeg-info ; echo test ; rm -rf build ; rm -rf dist ;
-"$HOME"/miniconda3/bin/python -m bumpversion micro
+"$HOME"/miniconda3/bin/python -m bumpversion patch setup.py # minor,major
 #"$HOME"/miniconda3/bin/python -m incremental.update mlib --patch
 "$HOME"/miniconda3/bin/python setup.py sdist bdist_wheel
 "$HOME"/miniconda3/bin/python -m twine check dist/*
