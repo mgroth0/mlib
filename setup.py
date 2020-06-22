812505bb4757301a7a2404fe5dcf73c190878ba3
Matt Groth <mgroth49@gmail.com>
2020-06-22 15:17:10 -0400

    auto-commit build cfgs

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="mlib-mgroth0",
-    version="0.0.4",
+    version="0.0.5",
     author="Matt Groth",
     author_email="mjgroth@mit.edu",
     description="Matt's lib",
