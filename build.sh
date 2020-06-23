ef0a1b6059fa0c921a9c196d73e303b153db9e9d
Matt Groth <mgroth49@gmail.com>
2020-06-22 20:07:59 -0400

    pw buildscript

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ pwd
 echo "build.sh: ls:"
 ls -a
 #"$HOME"/miniconda3/envs/build/bin/python setup.py sdist bdist_wheel
-echo "build.sh: conda install"
-conda install --file reqs_conda.txt
+#echo "build.sh: conda install"
+#conda install --file reqs_conda.txt
 echo "build.sh: pip install"
 python -m pip install --no-deps --ignore-installed .
 #"$HOME"/miniconda3/envs/build/bin/python -m twine check dist/*
