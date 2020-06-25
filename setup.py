86e5e442b8f31440f4e27d414e3b6f46dcf58346
Matt Groth <mgroth49@gmail.com>
2020-06-25 02:48:20 -0400

    auto-commit build cfgs

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
 
             
-        import setuptools
-        
-        with open("README.md", "r") as fh:
-            long_description = fh.read()
-        
-        setuptools.setup(
-            name="mlib-mgroth0",
-            version="0.0.44",
-        author="Matt Groth",
-        author_email="mjgroth@mit.edu",
-        description="Matt's lib",
-        long_description=long_description,
-        long_description_content_type="text/markdown",
-        url="https://github.com/mgroth0/mlib",
-        packages=setuptools.find_packages(),
-        classifiers=[
-            "Programming Language :: Python :: 3.8",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: MacOS :: MacOS X",
-        ],
-        python_requires='>=3.8',
-    )
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+name="mlib-mgroth0",
+version="0.0.45",
+author="Matt Groth",
+author_email="mjgroth@mit.edu",
+description="Matt's lib",
+long_description=long_description,
+long_description_content_type="text/markdown",
+url="https://github.com/mgroth0/mlib",
+packages=setuptools.find_packages(),
+classifiers=[
+"Programming Language :: Python :: 3.8",
+"License :: OSI Approved :: MIT License",
+"Operating System :: MacOS :: MacOS X",
+],
+python_requires='>=3.8',
+)
     
         
     
