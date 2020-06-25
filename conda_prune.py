4d38e01b715fc32fa34c096b3edc38db1933c74a
Matt Groth <mgroth49@gmail.com>
2020-06-25 02:34:38 -0400

    pw prune

--- conda_prune.py
+++ conda_prune.py
@@ -1,14 +1,14 @@
-import subprocess
-
 from mlib.boot import log
+from mlib.boot.mutil import err
 from mlib.file import strippedlines, load, File
-from mlib.shell import shell, spshell
+from mlib.shell import spshell
 def conda_prune():
     # sometimes there are circular deps because of older versions. I'll just have to 'stop'
     tops = strippedlines('reqs_conda_top.txt').tolist()
     subs = strippedlines('reqs_conda_sub.txt').tolist()
     cache = load('reqs_conda_cache.json')
     stop = False
+    skipped = False
     checking = []
     def check(req):
         my_req_short = req.split('=')[0]
@@ -16,7 +16,7 @@ def conda_prune():
             # handles when a package is a sub twice and recursives
             log(f'already checking {my_req_short}')
             return
-        stp = False
+        stp = skped = False
         checking.append(req)
         log(f'checking: {my_req_short}')
         if line in list(cache.keys()):
@@ -58,33 +58,40 @@ def conda_prune():
                 log(f'{d} is sub')
             else:
                 log(f'{my_req_short} depends on {d}')
-                my_a = input('top?sub?stop? > ')
+                my_a = input('top?sub?stop/skip? > ')
                 if my_a == 'top':
                     log('adding top')
                     tops.append(d)
                 elif my_a == 'sub':
                     log(f'recursing into {d}')
-                    stp = check(d)
+                    stp, skp = check(d)
+                    if skp:
+                        skped = True
                     log('adding sub')
                     subs.append(d)
                 elif my_a == 'stop':
                     stp = True
                     break
+                elif a == 'skip':
+                    skped = True
+                else:
+                    err('bad answer')
 
         if len(deps) == 0:
             log(f'{my_req_short} has 0 deps')
 
-        return stp
+        return stp, skped
 
 
     lines = strippedlines('reqs_conda.txt').filtered(
         lambda l: not l.strip().startswith('#')
     )
 
-
     for line in lines:
         if stop: break
-        stop = check(line)
+        stop,skp = check(line)
+        if skp:
+            skipped=True
 
     for line in lines:
         if stop:
@@ -92,7 +99,7 @@ def conda_prune():
         req_short = line.split('=')[0].strip()
         if req_short not in tops and req_short not in subs:
             log(f'never answered for {req_short}')
-            a = input('top?sub?stop? > ')
+            a = input('top?sub?stop/skip? > ')
             if a == 'top':
                 log('adding top')
                 tops.append(req_short)
@@ -102,9 +109,13 @@ def conda_prune():
             elif a == 'stop':
                 stop = True
                 break
+            elif a == 'skip':
+                skipped = True
+            else:
+                err('bad answer')
 
     File('reqs_conda_top.txt').write_lines(set(tops))
     File('reqs_conda_sub.txt').write_lines(set(subs))
     File('reqs_conda_cache.json').save(cache)
 
-    return stop
+    return not stop and not skipped
