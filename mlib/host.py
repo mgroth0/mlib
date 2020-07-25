4f8747fa2e62cca19bf738090f33f79b9693a9d1
Matt Groth <mgroth49@gmail.com>
2020-07-25 02:01:27 -0400

    auto-commit 4 singularity

--- mlib/host.py
+++ mlib/host.py
@@ -82,6 +82,7 @@ class HostProject(ABC):
 
         if isinstance(self, OpenMindProject):
             log('upping')
+            breakpoint()
             p.sendatprompt('vagrant up')
             log('sshing')
             p.sendatprompt('vagrant ssh')
