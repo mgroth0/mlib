835ac95dae9fd90cff36c7107fc04971ed2a7205
Matt Groth <mgroth49@gmail.com>
2020-07-25 01:46:05 -0400

    auto-commit 4 singularity

--- mlib/host.py
+++ mlib/host.py
@@ -80,6 +80,7 @@ class HostProject(ABC):
             p.sendatprompt('vagrant up')
             p.sendatprompt('vagrant shh')
             p.setprompt()
+            p.prompt() #an extra prompt expect like in the build process i think
             p.sendatprompt('cd ../dnn')
         p.sendatprompt(
             f'sudo bash {SW.name}')  # why was I using sudo??? ohhhh I might have been using sudo in order to have write access to files? yes!! I was suing sudo because that is the only way files are writable!
