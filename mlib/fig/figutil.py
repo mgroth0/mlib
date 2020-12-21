3963c8ad26516fd4380da2199e5310740b38228e
Matt Groth <mgroth49@gmail.com>
2020-12-21 11:59:43 -0500

    auto-commit 4 singularity

--- mlib/fig/figutil.py
+++ mlib/fig/figutil.py
@@ -6,6 +6,7 @@ def add_headers_to_mat(ar2d, row_headers, col_headers, alphabetize=False):
     ar2d = np.concatenate((make2d(col_headers), arr(ar2d)), axis=0)
     row_headers = make2d(np.insert(row_headers, 0, None)).T
     cmat = np.concatenate((row_headers, ar2d), axis=1)
+    breakpoint()
     if alphabetize:
         cmat[1:] = sort_human(cmat[1:])
         temp = np.transpose(arr(cmat))
